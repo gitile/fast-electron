ba08b24319051be459b896b99431a92a94ec795c
tulingfeng <xtotly@gamil.com>
2021-01-27 13:52:12 +0800

    完善代码

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,113 +4,102 @@ const fs = require("fs");
 var $ = require('jquery');
 
 var FastWebView = {
-	initUrl: '',
-	lastUrl: '',
-	isMax: false,
-	init: function() {
-		let _this = this;
-		// 获取到对应的webview 
-		var webview = $('#browser');
-		webview.on('dom-ready', function () {
-			// 当页面载入完成，更新toolbar
-			webview.show();
+    initUrl: '',
+    lastUrl: '',
+    init: function() {
+        let _this = this;
+        // 获取到对应的webview 
+        var webview = $('#browser');
+        webview.on('dom-ready', function () {
+            // 当页面载入完成，更新toolbar
+            webview.show();
+        });
+        webview.on('did-start-loading', function () {
+            // 更新进度条以及刷新按钮变为loading的图示，表示载入中
+            _this.startLoading();
+        });
+        webview.on('did-stop-loading', function () {
+            // 停止加载显示
+            _this.stopLoading();
+        });
+        webview.on('did-stop-loading', function () {
+            // 停止加载显示
+            _this.stopLoading();
+        });
+        webview[0].addEventListener('did-navigate', (res) => {
+            _this.lastUrl = webview.attr("src")
+        });
+		// 网页打开新窗口操作
+        webview[0].addEventListener('new-window', (e) => {
+            let url = e.url;
+            if(url&&url.length>0) {
+                if(url.startsWith("http://")||url.startsWith("https://")) {
+                    webview.attr("src", url)
+                }
+            }
+        });
+		// 右上角关闭、最大化、最小化操作
+        $("#minWindowNav").click(function(){
+            ipcRenderer.send('window-action', 'min')
+        });
+        $("#maxWindowNav").click(function(){
+            ipcRenderer.send('window-action', 'max')
+        });
+        $("#resetWindowNav").click(function(){
+            ipcRenderer.send('window-action', 'max')
+        });
+        $("#closeWindowNav").click(function(){
+           ipcRenderer.send('window-action', 'close')
+        });
+        $("#refreshWindowNav").click(function(){
+            if(_this.lastUrl&&_this.lastUrl.length>0) {
+                _this.jumpUrl(_this.lastUrl)
+            } else {
+                _this.jumpUrl(_this.initUrl)
+            }
+        });
+		// 监听主界面发送指令
+		ipcRenderer.on('jumpHomeUrl', (event, message) => {
+		    _this.jumpHomeUrl();
 		});
-		webview.on('did-start-loading', function () {
-			// 更新进度条以及刷新按钮变为loading的图示，表示载入中
-			_this.startLoading();
-		});
-		webview.on('did-stop-loading', function () {
-			// 停止加载显示
-			_this.stopLoading();
-		});
-		webview.on('did-stop-loading', function () {
-			// 停止加载显示
-			_this.stopLoading();
-		});
-		webview[0].addEventListener('did-navigate', (res) => {
-			_this.lastUrl = webview.attr("src")
-		});
-		webview[0].addEventListener('new-window', (e) => {
-			let url = e.url;
-			if(url&&url.length>0) {
-				if(url.startsWith("http://")||url.startsWith("https://")) {
-					webview.attr("src", url)
-				}
-			}
-		});
-		$("#minWindowNav").click(function(){
-			_this.min()
-		});
-		$("#maxWindowNav").click(function(){
-			_this.isMax = true
-			_this.max()
+		ipcRenderer.on('maximize', (event, message) => {
 			$("#maxWindowNav").hide();
 			$("#resetWindowNav").show();
 		});
-		$("#resetWindowNav").click(function(){
-			_this.isMax = false
-			_this.reset()
-			$("#maxWindowNav").show();
-			$("#resetWindowNav").hide();
+		ipcRenderer.on('unmaximize', (event, message) => {
+		    $("#maxWindowNav").show();
+		    $("#resetWindowNav").hide();
 		});
-		$("#closeWindowNav").click(function(){
-			_this.close()
-		});
-		$("#refreshWindowNav").click(function(){
-			if(_this.lastUrl&&_this.lastUrl.length>0) {
-				_this.setUrl(_this.lastUrl)
-			} else {
-				_this.setUrl(_this.initUrl)
-			}
-		});
-		$("#homeWindowNav").click(function(){
-			_this.homeUrl()
-		});
-		_this.readConfigFile(function(){
-			_this.homeUrl();
-		});
-		
-	},
-	readConfigFile: function(callback) {
-		let _this = this;
-		let configFilePath = path.join(__dirname, '../config.json')
-		fs.exists(configFilePath, function (exists) {
-			if (exists) {
-			  //读取本地的json文件
-			  let result = $.parseJSON(fs.readFileSync(configFilePath));
-			  if(result) {
-				  _this.initUrl = result.initUrl;
-				  (callback && typeof(callback) === "function") && callback();
-			  }
-			}
-		})
-	},
-	startLoading: function() {
-		$("#refreshImage").addClass("refresh-loading");
-	},
-	stopLoading: function() {
-		$("#refreshImage").removeClass("refresh-loading");
-	},
-	homeUrl: function() {
-		this.setUrl(this.initUrl);
-	},
-	setUrl: function(url) {
-		$('#browser').attr("src", url)
-	},
-	max: function() {
-		ipcRenderer.send('window-action', 'max')
-	},
-	min: function() {
-		ipcRenderer.send('window-action', 'min')
-	},
-	reset: function() {
-		ipcRenderer.send('window-action', 'reset')
-	},
-	close: function() {
-		ipcRenderer.send('window-action', 'close')
-	}
+        _this.jumpHomeUrl();
+    },
+    readConfigFile: function(callback) {
+        let _this = this;
+        let configFilePath = path.join(__dirname, '../config.json')
+        fs.exists(configFilePath, function (exists) {
+            if (exists) {
+              //读取本地的json文件
+              let result = $.parseJSON(fs.readFileSync(configFilePath));
+              if(result) {
+                  _this.initUrl = result.initUrl;
+                  (callback && typeof(callback) === "function") && callback();
+              }
+            }
+        })
+    },
+    startLoading: function() {
+        $("#refreshImage").addClass("refresh-loading");
+    },
+    stopLoading: function() {
+        $("#refreshImage").removeClass("refresh-loading");
+    },
+    jumpHomeUrl: function() {
+        this.jumpUrl(this.initUrl);
+    },
+    jumpUrl: function(url) {
+        $('#browser').attr("src", url)
+    }
 }
-
-$(document).ready(function(){ 
-	FastWebView.init()
+// 加载配置文件
+FastWebView.readConfigFile(function(){
+    FastWebView.init()
 });
--- main.js
+++ main.js
@@ -1,128 +1,150 @@
 const path = require('path')
 const {app, BrowserWindow, ipcMain, Menu, Tray, remote} = require('electron')
 
-var FastOA = {
-	appName: '火速应用',
-	mainWindow: null,
-	appIcon: null,
-	debug: false,
-	windowOptions: {
-		width: 1200,
-		height: 760,
-		minHeight: 700,
-		minWidth: 1000,
-		frame: false,
-		webPreferences: {
-			nodeIntegration: true,
-			webviewTag: true
-		}
-	},
-	init: function() {
-		let _this = this;
-		_this.makeSingleInstance()
-		// 判断是否开启debug模式
-		_this.debug = /--debug/.test(process.argv[2]);
-		
-		app.on('ready', () => {
-		  _this.createWindow()
+var FastApp = {
+    appName: '火速应用',
+    mainWindow: null,
+    appIcon: null,
+    debug: false,
+    windowOptions: {
+        width: 1200,
+        height: 760,
+        minHeight: 700,
+        minWidth: 1000,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true
+        }
+    },
+    init: function() {
+        let _this = this;
+        // 判断是否开启debug模式
+        _this.debug = /--debug/.test(process.argv[2]);
+        app.on('ready', () => {
+          _this.createWindow()
+        })
+        // 二次启动是监听
+        app.on('second-instance', (event, commandLine, workingDirectory) => {
+            // 当运行第二个实例时,将会聚焦到这个窗口
+            if (_this.mainWindow) {
+              if (_this.mainWindow.isMinimized()) _this.mainWindow.restore()
+              _this.mainWindow.focus()
+            }
+        })
+        app.on('window-all-closed', () => {
+          if (process.platform !== 'darwin') {
+            app.quit()
+          }
+        })
+        app.on('activate', () => {
+          if (_this.mainWindow === null) {
+            _this.createWindow()
+          }
+        })
+        // 创建Ipc通讯
+         _this.initIpc()
+    },
+    initIpc: function() {
+        //主进程渲染器操作
+        let _this = this;
+        ipcMain.on('window-action', function(event, arg) {
+          if(arg=='max') {
+              _this.maximizeWindow()
+          } else if(arg=='min') {
+              _this.minimizeWindow()
+          } else if(arg=='close') {
+              _this.hideWindow()
+          }
+        });
+    },
+    initTray: function() {
+        let _this = this;
+        var iconPath = path.join(__dirname, '/assets/images/logo.png')
+        this.appIcon = new Tray(iconPath)
+        var contextMenu = Menu.buildFromTemplate([{
+            label: '打开界面',
+            icon: path.join(__dirname, '/assets/images/icon-tray-back.png'),
+            click: () => {
+                if(!_this.mainWindow.isVisible()) {
+                    _this.mainWindow.show();
+                } else {
+                    if (_this.mainWindow.isMinimized()) _this.mainWindow.restore()
+                    _this.mainWindow.focus()
+                }
+            }
+        },{
+            label: '返回主页',
+            icon: path.join(__dirname, '/assets/images/icon-tray-home.png'),
+            click: () => {
+				// 向页面发送访问主页命令
+                this.mainWindow.webContents.send('jumpHomeUrl');
+            }
+        },{
+            label: '退出系统',
+            icon: path.join(__dirname, '/assets/images/icon-tray-signout.png'),
+            click: () => {
+                if (process.platform !== 'darwin') {
+                  app.quit()
+                }
+            }
+        }])
+        _this.appIcon.setToolTip(this.appName)
+        _this.appIcon.setContextMenu(contextMenu)
+        _this.appIcon.on('click', ()=>{
+            _this.mainWindow.isVisible()?_this.mainWindow.hide():_this.mainWindow.show();
+            _this.mainWindow.isVisible()?_this.mainWindow.setSkipTaskbar(false):_this.mainWindow.setSkipTaskbar(true);
+        })
+    },
+    createWindow: function() {
+        //隐藏菜单栏
+        Menu.setApplicationMenu(null)
+        // 创建窗口
+        this.windowOptions.icon = path.join(__dirname, '/assets/images/logo.png')
+        this.mainWindow = new BrowserWindow(this.windowOptions)
+        this.mainWindow.loadURL(path.join(__dirname, '/index.html'))
+        this.mainWindow.on('closed', () => {
+            this.mainWindow = null
+        })
+		this.mainWindow.on('maximize', () => {
+		    this.mainWindow.webContents.send('maximize');
 		})
-		app.on('window-all-closed', () => {
-		  if (process.platform !== 'darwin') {
-		    app.quit()
-		  }
+		this.mainWindow.on('unmaximize', () => {
+		    this.mainWindow.webContents.send('unmaximize');
 		})
-		app.on('activate', () => {
-		  if (_this.mainWindow === null) {
-		    _this.createWindow()
-		  }
-		})
-		 _this.initIpc()
-	},
-	initIpc: function() {
-		//主进程渲染器操作
-		let _this = this;
-		ipcMain.on('window-action', function(event, arg) {
-		  if(arg=='max') {
-		  	_this.maximizeWindow()
-		  } else if(arg=='min') {
-		  	_this.minimizeWindow()
-		  } else if(arg=='close') {
-		  	_this.closeWindow()
-		  } else if(arg=='reset') {
-		  	_this.resetWindow()
-		  }
-		});
-	},
-	initTray: function() {
-		var iconPath = path.join(__dirname, '/assets/images/logo.png')
-		this.appIcon = new Tray(iconPath)
-		var contextMenu = Menu.buildFromTemplate([{
-		    label: '退出系统',
-		    click: () => {
-				if (process.platform !== 'darwin') {
-				  app.quit()
-				}
-		    }
-		}])
-		this.appIcon.setToolTip(this.appName)
-		this.appIcon.setContextMenu(contextMenu)
-		let _this = this;
-		_this.appIcon.on('click', ()=>{
-			//我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
-		    _this.mainWindow.isVisible()?_this.mainWindow.hide():_this.mainWindow.show();
-			_this.mainWindow.isVisible()?_this.mainWindow.setSkipTaskbar(false):_this.mainWindow.setSkipTaskbar(true);
-		})
-	},
-	createWindow: function() {
-		// 创建窗口
-		if (process.platform === 'linux') {
-		  this.windowOptions.icon = path.join(__dirname, '/assets/images/logo.png')
+        // 初始化托盘
+        this.initTray()
+		// 开发模式打开调试工具
+		if(this.debug) {
+		    this.mainWindow.webContents.openDevTools()
+		    this.mainWindow.maximize()
 		}
-		this.mainWindow = new BrowserWindow(this.windowOptions)
-		this.mainWindow.loadURL(path.join(__dirname, '/index.html'))
-		if (this.debug) {
-		  this.mainWindow.webContents.openDevTools()
-		  this.mainWindow.maximize()
-		}
-		this.mainWindow.on('closed', () => {
-		  this.mainWindow = null
-		})
-		 this.initTray()
-	},
-	minimizeWindow: function() {
-		if (this.mainWindow) {
-			this.mainWindow.minimize();
-		}
-	},
-	maximizeWindow: function() {
-		if (this.mainWindow) {
-			if(!this.mainWindow.isMaximized()) this.mainWindow.maximize();
-		}
-	},
-	resetWindow: function() {
-		if (this.mainWindow) {
-			if(this.mainWindow.isMaximized()) this.mainWindow.unmaximize()
-		}
-	},
-	closeWindow: function() {
-		if (this.mainWindow) {
-			this.mainWindow.hide();
-		}
-	},
-	makeSingleInstance: function() {
-		if (process.mas) return
-		app.requestSingleInstanceLock()
-		let _this = this;
-		app.on('second-instance', () => {
-		    if (_this.mainWindow) {
-		      if (_this.mainWindow.isMinimized()) _this.mainWindow.restore()
-		      _this.mainWindow.focus()
-		    }
-		})
-	}
+    },
+    minimizeWindow: function() {
+        if (this.mainWindow) {
+            this.mainWindow.minimize();
+        }
+    },
+    maximizeWindow: function() {
+        if (this.mainWindow) {
+			this.mainWindow.isMaximized()?this.mainWindow.unmaximize():this.mainWindow.maximize();
+        }
+    },
+    hideWindow: function() {
+        if (this.mainWindow) {
+            this.mainWindow.hide();
+        }
+    }
 }
+// 判断是否单例
+const gotTheLock = app.requestSingleInstanceLock()
 
-FastOA.init()
+if (!gotTheLock) {
+    app.quit()
+} else {
+    // 创建新Window
+    FastApp.init()
+}
 
 
 
